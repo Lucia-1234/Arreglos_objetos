2d88f2637108c527ebb2053391114d06840b82ee
Valentina serrano <valentinaserrano120@gmail.com>
2026-02-19 17:38:18 -0500

    se agrego el punto c de contextualizacion

---
+++ contextualizacion/punto_c/app.js
@@ -0,0 +1,12 @@
+let libro={
+    titulo:"La chica del tren",
+    autor: "Paula hawkins",
+    año: 2015,
+    genero: "Suspenso"
+}
+
+console.log(libro.titulo);
+console.log(libro.autor);
+console.log(libro.año);
+console.log(libro.genero);
+
