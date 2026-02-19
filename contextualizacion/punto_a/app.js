8bc22a1e9f001b74ec396b7bfb98d74b207b0b26
michelle_roa <roakaren2507@gmail.com>
2026-02-19 17:52:58 -0500

    se agregaron impresiones por consola

--- contextualizacion/punto_a/app.js
+++ contextualizacion/punto_a/app.js
@@ -4,14 +4,22 @@ let frutas = ["Manzana", "Pera", "Banano"]
 //agrega un elemento al final de la lista
 frutas.push("Mango");
 
+console.log(frutas);
+
 //se elimina el ultimo elemento de la lista
 frutas.pop();
+
+console.log(frutas);
 
 //se agrega un elemento al inicio de la lista
 frutas.unshift("uva");
 
+console.log(frutas);
+
 //se elimina un elemento al inicio de la lista
 frutas.shift();
+
+console.log(frutas);
 
 //se cuentan los elementos de la lsita
 console.log(frutas.length);
